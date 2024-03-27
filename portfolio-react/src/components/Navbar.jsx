685be2c71217b78ede6b8841a982f056333ee1e4
David Lopez <101904205+deivi2401@users.noreply.github.com>
2024-03-27 03:44:55 -0500

    Primera implementacion de la pagina del portafolio
    En este commit termine de escribir todo el codigo base del portafolio, quedan por hacer:
    
    [ ] Eliminar el menu horrible del lateral de las redes sociales
    [ ] Agregar la conexion a mi API personal(TERMINAR DE VER LOS TUTORIALES!!!)  donde agregare mis imagenes y datos actualizables

--- portfolio-react/src/components/Navbar.jsx
+++ portfolio-react/src/components/Navbar.jsx
@@ -10,6 +10,7 @@ import {
 import { HiOutlineMail } from "react-icons/hi";
 import { BsFillPersonLinesFill } from "react-icons/bs";
 import Logo from "./Logo";
+import { Link } from "react-scroll";
 
 export const Navbar = () => {
   const [nav, setNav] = useState(false);
@@ -21,15 +22,41 @@ export const Navbar = () => {
      px-4 bg-[#0a192f] text-gray-300"
     >
       <div className="w-12">
-        <Logo src="https://media.tenor.com/I6eTnjTjfbsAAAAi/shocked-david-martinez.gif" alt="Logo" />
+        <Logo
+          src="https://media.tenor.com/I6eTnjTjfbsAAAAi/shocked-david-martinez.gif"
+          alt="Logo"
+        />
       </div>
       {/*menu*/}
       <ul className="hidden md:flex">
-        <li>Home</li>
-        <li>About</li>
-        <li>Skills</li>
-        <li>Work</li>
-        <li>Contact</li>
+        <li>
+          <Link to="home" spy={true} smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="about" spy={true} smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="skills" spy={true} smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          {" "}
+          <Link to="work" spy={true} smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" spy={true} smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
       {/*hamburguer*/}
@@ -45,11 +72,31 @@ export const Navbar = () => {
             : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
         }
       >
-        <li className="py-6 text-4xl">Home</li>
-        <li className="py-6 text-4xl">About</li>
-        <li className="py-6 text-4xl">Skills</li>
-        <li className="py-6 text-4xl">Work</li>
-        <li className="py-6 text-4xl">Contact</li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick} to="home" spy={true} smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick}  to="about" spy={true} smooth={true} duration={500}>
+            About
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick}  to="skills" spy={true} smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick}  to="work" spy={true} smooth={true} duration={500}>
+            Work
+          </Link>
+        </li>
+        <li className="py-6 text-4xl">
+          <Link onClick={handleClick}  to="contact" spy={true} smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
       {/* Social icons */}
       <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
