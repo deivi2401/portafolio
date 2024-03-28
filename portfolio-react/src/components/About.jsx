183cbfafd0a8960d25d8da9d66ef2e91007bd180
David Lopez <101904205+deivi2401@users.noreply.github.com>
2024-03-27 20:08:50 -0500

    Implementacion de los iconos de redes sociales
    Se decidio cambiar el diseno de los simbolos de redes a algo mas simple y no tan intrusivo

--- portfolio-react/src/components/About.jsx
+++ portfolio-react/src/components/About.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { Reveal } from "./Reveal";
 
 export const About = () => {
   return (
@@ -6,24 +7,31 @@ export const About = () => {
       <div className="flex flex-col justify-center items-center w-full h-full">
         <div className="max-w-[1000px] w-full px-4 grid grid-cols-2 gap-8">
           <div className="sm:text-right pb-8 pl-4">
-            <p className="text-4xl font-bold inline border-b-4 border-pink-600">
-              About
-            </p>
+            <Reveal>
+              <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+                About
+              </p>
+            </Reveal>
           </div>
         </div>
         <div className="max-w-[1000px] w-full grid sm:grid-cols-2 gap-8 px-4">
           <div className="sm:text-right text-4xl font-bold">
-            <p>
-              Hi my name is David, nice to meet you. Please take a look around.{" "}
-            </p>
+            <Reveal>
+              <p>
+                Hi my name is David, nice to meet you. Please take a look
+                around.{" "}
+              </p>
+            </Reveal>
           </div>
           <div>
-            <p>
-              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet
-              beatae facere adipisci reiciendis nobis temporibus numquam quae
-              voluptate nesciunt molestias quas dolore quibusdam voluptas, eum
-              veritatis saepe, distinctio ullam laboriosam!
-            </p>
+            <Reveal>
+              <p>
+                Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                Eveniet beatae facere adipisci reiciendis nobis temporibus
+                numquam quae voluptate nesciunt molestias quas dolore quibusdam
+                voluptas, eum veritatis saepe, distinctio ullam laboriosam!
+              </p>
+            </Reveal>
           </div>
         </div>
       </div>
--- portfolio-react/src/components/Skills.jsx
+++ portfolio-react/src/components/Skills.jsx
@@ -10,10 +10,10 @@ const Skills = () => {
   return (
     <div name="skills" className="bg-[#0a192f] text-gray-300">
       <div>
-        <p className="text-4xl font-bold inline border-b-4 border-pink-600">
+        <p className="text-4xl font-bold inline border-b-4 border-pink-600 m-6">
           Experience
         </p>
-        <p className="py-4">// I've worked with the following technologies</p>
+        <p className="py-4 m-6">// I've worked with the following technologies</p>
       </div>
       <div className="w-full grid grid-cols-2 sm:grid-cols-4 gap-4 text-center py-8">
         <div className="shadow-md shadow-[#040c16] hover:scale-110 duration-300">
