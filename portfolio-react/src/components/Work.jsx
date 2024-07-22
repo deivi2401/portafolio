9e6b1d92ecd60df9dcb611c7ee450aa0447ce993
deivi2401 <101904205+deivi2401@users.noreply.github.com>
2024-07-22 11:39:37 -0600

    Portfolio
    Se añadio el proyecto del portafolio a la pagina

--- portfolio-react/src/components/Work.jsx
+++ portfolio-react/src/components/Work.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import BudgetApp from "../assets/Budget-app.png";
-import RealState from "../assets/realestate.jpg";
+import Portfolio from "../assets/Portfolio.png";
 import QRPet from "../assets/QRPet.jpeg";
 
 const Work = () => {
@@ -58,6 +58,31 @@ const Work = () => {
                   </button>
                 </a>
                 <a href="https://github.com/deivi2401/QRPET">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Code
+                  </button>
+                </a>
+              </div>
+            </div>
+          </div>
+
+                      {/* Grid Item */}
+                      <div
+            style={{ backgroundImage: `url(${Portfolio})` }}
+            className="shadow-lg shadow-[#040c16] group container rounde-md flex justify-center items-center mx-auto content-div">
+            {/*Hover effects */}
+
+            <div className="opacity-0 group-hover:opacity-100">
+              <span className="text-2xl font-bold text-white tracking-wider">
+                React JS Application
+              </span>
+              <div className="pt-8 text-center">
+              <a href="https://deivi2401.github.io/portfolio-app/">
+                  <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
+                    Demo
+                  </button>
+                </a>
+                <a href="https://github.com/deivi2401/portafolio">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Code
                   </button>
